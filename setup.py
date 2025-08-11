6211cc0530c9f4c1ab3698ddfaad7ce60d0ff10b
Padhai_Kanner <67086525+djbravo06@users.noreply.github.com>
2025-08-11 19:35:53 +0530

    Fixed the code base for newer version of the modules

--- setup.py
+++ setup.py
@@ -1,30 +1,36 @@
 from setuptools import setup, find_packages
-from codecs import open
 from os import path
 
 __version__ = '0.0.4'
 
 here = path.abspath(path.dirname(__file__))
 
-# get the dependencies and installs
+# Get the dependencies and installs
 with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
-    all_reqs = f.read().split('\n')
+    all_reqs = [
+        x.strip()
+        for x in f.read().splitlines()
+        if x.strip() and not x.startswith('#')
+    ]
 
-install_requires = [x.strip() for x in all_reqs if 'git+' not in x]
-dependency_links = [x.strip().replace('git+', '') for x in all_reqs if x.startswith('git+')]
+install_requires = [x for x in all_reqs if not x.startswith('git+')]
+dependency_links = [x.replace('git+', '') for x in all_reqs if x.startswith('git+')]
 
 setup(
     name='mlfromscratch',
     version=__version__,
-    description='Python implementations of some of the fundamental Machine Learning models and algorithms from scratch.',
+    description=(
+        'Python implementations of some of the fundamental Machine Learning '
+        'models and algorithms from scratch.'
+    ),
     url='https://github.com/eriklindernoren/ML-From-Scratch',
     download_url='https://github.com/eriklindernoren/ML-From-Scratch/tarball/master',
     license='MIT',
-    packages=find_packages(),
+    packages=find_packages(exclude=['.venv', 'tests', 'tests.*']),
     include_package_data=True,
     author='Erik Linder-Noren',
-    install_requires=install_requires,
-    setup_requires=['numpy>=1.10', 'scipy>=0.17'],
+    author_email='eriklindernoren@gmail.com',
+    install_requires=install_requires + ['numpy>=1.10', 'scipy>=0.17'],
     dependency_links=dependency_links,
-    author_email='eriklindernoren@gmail.com'
+    python_requires='>=3.7',
 )
